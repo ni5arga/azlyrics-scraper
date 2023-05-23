c8fe51c2b028eeb25e6cd39d875a2dc3a9eb9c57
Nisarga Adhikary <git@nisarga.me>
2023-05-23 19:48:03 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,18 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-# URL for the song lyrics on azlyrics.com
-# Bille Jean for the example xD, can be anyone song you want to fetch
-
 url = "https://www.azlyrics.com/lyrics/michaeljackson/billiejean.html"
 
-# Send a GET request to the URL and parse the HTML content
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# Extract the lyrics from the HTML tags
-lyrics_div = soup.find('div', class_='col-xs-12 col-lg-8 text-center')
-lyrics = lyrics_div.find_all('div')[6].get_text()
+lyrics_div = soup.find('div', class_='ringtone')
+lyrics = lyrics_div.find_next_sibling('div').text.strip()
 
-# Print the lyrics
 print(lyrics)
